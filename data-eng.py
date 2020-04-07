7d345538147ef96a1e2859d971d64c98bc958d00
Ivo Nutar <ivo.nutar@gmail.com>
2020-04-07 19:26:39 +0200

    Dealt with outliers in float,int features

--- data-eng.py
+++ data-eng.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 train_data = pandas.read_csv('data/train.csv')
 
 
-for feature in train_data.keys():
-    if train_data[feature].dtype in [np.float, np.int]:
-        train_data.boxplot(column=feature)
-        plt.show()
+# for feature in train_data.keys():
+#     if train_data[feature].dtype in [np.float, np.int]:
+#         train_data.boxplot(column=feature)
+#         plt.show()
 
 
 outliers = ['LotFrontage',
@@ -19,7 +19,16 @@ outliers = ['LotFrontage',
             'BsmtFinSF2',
             'TotalBsmtSF',
             '1stFlrSF',
-            'LowQualFinSF'
-            'GrLivArea'
+            'LowQualFinSF',
+            'GrLivArea',
             'OpenPorchSF']
 
+for feature in outliers:
+    col = train_data[feature]
+    col = col[col.between(col.quantile(.15), col.quantile(.85))]
+    train_data[feature] = col
+
+for feature in outliers:
+    if train_data[feature].dtype in [np.float, np.int]:
+        train_data.boxplot(column=feature)
+        plt.show()
